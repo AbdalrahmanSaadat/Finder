8426847d0bf9491d14316f9e9047f69499266904
Abdalrahman Saadat <139587515+AbdalrahmanSaadat@users.noreply.github.com>
2023-11-08 14:51:21 +0200

    publish the first branch and adding word count feature

---
+++ word-count.py
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#work on counting function
+#file input
+fhand = (open("test.txt"))
+l_fhand = list(fhand)
+
+#loop around the file
+line_count = 0
+word_count = 0
+
+for l in l_fhand:
+    l = l.rstrip()
+    line_count += 1
+    for w in l:
+        word_count +=1
+
+#output
+print("Line count:",line_count)
+print("Word count:",word_count)
+
